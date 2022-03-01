b688c8009fb7c3fa86a33ea5a3753d1076abd4f1
Andrew <shamrockdev7@gmail.com>
2022-02-28 18:28:43 -0700

    added form and header components

---
+++ src/modules/Form/Form.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Form.module.scss'
+
+const Form: React.FC = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
---
+++ src/modules/Table/Table.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../../state/hooks';
+import styles from './Table.module.scss'
+
+
+const Table: React.FC = () => {
+
+
+  return (
+    <div>
+      TABLE
+    </div>
+  );
+}
+
+
+export default Table
